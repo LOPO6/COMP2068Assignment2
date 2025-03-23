9a35a2f2ec05c718569397be9e63c9c159aa1dd1
Logan <lbpotopn@lakeheadu.ca>
2025-03-23 14:27:57 -0400

    Initialized angular site, added in components, built nav header with working links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    {path: 'list', component: ListComponent},
+    {path: '', component: HomeComponent},
+    {path: 'add', component: AddComponent}
+];
