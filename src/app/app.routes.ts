d7ccd87b5c3d24b2f1ea1760f768ff1ac86cf2f1
Logan <lbpotopn@lakeheadu.ca>
2025-03-31 16:56:59 -0400

    changed list component to shoe component to account for adding a new manufacturer component, allowing for better organization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AddComponent } from './components/add/add.component';
-import { ListComponent } from './components/list/list.component';
+import { ShoeComponent } from './components/shoe/shoe.component';
 export const routes: Routes = [
 
-    {path: 'list', component: ListComponent},
+    {path: 'shoe', component: ShoeComponent},
     {path: '', component: HomeComponent},
     {path: 'add', component: AddComponent}
 ];
--- src/app/components/shoe/shoe.component.ts
+++ src/app/components/shoe/shoe.component.ts
@@ -1,36 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { ShoeService } from '../../services/shoe.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-list',
-  imports: [NgFor, NgIf, FormsModule],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
-})
-
-export class Shoe{
-  _id: string | undefined;
-  name: string | undefined;
-  price: number | undefined;
-  category: string | undefined;
-
-
-  manufacturer?: {
-    name: string | undefined;
-    foundingYear: number | undefined;
-  };
-  
-}
-
-@Component({
   selector: 'app-Shoe',
   imports: [NgFor, NgIf, FormsModule],
-  templateUrl: './list.component.html'
+  templateUrl: './shoe.component.html'
 })
 
-export class ListComponent implements OnInit{
+export class ShoeComponent {
   SHOES: any;
   _id: string | undefined;
   name: string | undefined;
